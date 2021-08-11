33cef4304ab39983b1c37a2bc42f994690c3087f
Blake Enloe <blakewenloe@yahoo.com>
2021-08-10 20:44:22 -0500

    added location marker

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react";
+import { useMap } from "react-leaflet";
 import axios from "axios";
 import Map from "./components/Map";
 
 function App() {
-  const [launchPads, setData] = useState();
+  const [satellites, setData] = useState();
+
+  const defaultCenter = [0, 0];
+  const defaultZoom = 4;
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await axios("https://api.spacexdata.com/v3/launchpads");
-      setData(response.data);
-    }
-    fetchData();
+    const updateLocations = async () => {
+      const response = await axios(
+        "http://3.143.45.11:3000/satellites/above-me/30.626945/-96.334334"
+      );
+      return setData(response.data);
+    };
+    updateLocations();
+    setInterval(updateLocations, 5000);
   }, []);
 
-  return <Map launchPads={launchPads}> </Map>;
+  return (
+    <Map
+      center={defaultCenter}
+      satellites={satellites}
+      zoom={defaultZoom}
+    ></Map>
+  );
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import SatIcon from "./satellite.png";
+import L from "leaflet";
+import LocationMarker from "./LocationMarker";
+
+const satIcon = L.icon({
+  iconUrl: SatIcon,
+  iconSize: [38, 38],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+});
 
 export default function Map(props) {
-  return props.launchPads ? (
+  const Satellites = () => {
+    return props.satellites
+      ? props.satellites.above.map((launchPad) => {
+          return (
+            <Marker
+              position={[launchPad.satlat, launchPad.satlng]}
+              key={launchPad["satid"]}
+              icon={satIcon}
+            >
+              <Popup>
+                <p class="landpad-details">
+                  Sat Name:
+                  <strong>{launchPad.satname}</strong>
+                  <br />
+                </p>
+              </Popup>
+            </Marker>
+          );
+        })
+      : "Map is loading...";
+  };
+  return (
     <MapContainer
-      center={[37.937141, -100.309096]}
+      center={[0, 0]}
       zoom={5}
       style={{ height: "100vh", width: "100vw" }}
     >
@@ -12,52 +43,8 @@ export default function Map(props) {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      ,
-      {props.launchPads.map((launchPad) => {
-        const point = [
-          launchPad["location"].latitude,
-          launchPad["location"].longitude,
-        ];
-
-        return (
-          <Marker position={point} key={launchPad["id"]}>
-            <Popup>
-              <p class="landpad-details">
-                Site Name:
-                <strong>
-                  {launchPad.site_name_long} ({launchPad.name})
-                </strong>
-                <br />
-                Status: <strong>{launchPad.status}</strong>
-                <br />
-                Attempted launches:
-                <strong>{launchPad.attempted_launches}</strong>
-                <br />
-                Successful launches:
-                <strong>{launchPad.successful_launches}</strong>
-                <br />
-                Vehicles Launched:
-                <ul>
-                  {launchPad.vehicles_launched.map((vehicle, i) => (
-                    <li key={i}>
-                      <strong>{vehicle}</strong>
-                    </li>
-                  ))}
-                </ul>
-                <p>
-                  <strong>Details</strong>: {launchPad.details}
-                </p>
-                Location:
-                <strong>
-                  {launchPad.location.name}, {launchPad.location.region}
-                </strong>
-              </p>
-            </Popup>
-          </Marker>
-        );
-      })}
+      <Satellites />
+      <LocationMarker />
     </MapContainer>
-  ) : (
-    "Data is loading..."
   );
 }
